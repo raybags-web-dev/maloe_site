c4dda535493240fa9cd88a86f48a7f819d304e04
raybags <baguma.github@gmail.com>
2021-08-15 14:08:11 +0200

    implimented navbar handler for each link, changed content layout (navlinks)

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,30 @@
+"use strict";
+const navBtn = document.querySelector("#navbar-toggler");
+const navDiv = document.querySelector(".navbar-collapse");
+const navLinks = Array.from(document.querySelectorAll(".navbar-nav .nav-link"));
 
-const navBtn = document.querySelector('#navbar-toggler');
-const navDiv = document.querySelector('.navbar-collapse');
+// aos animation initialization
+AOS.init();
+// show navbtn handler
+navBtn.addEventListener("click", () => {
+  navDiv.classList.add("showNav");
+  navDiv.classList.remove("hideNav");
+});
 
-navBtn.addEventListener('click', () => {
-    navDiv.classList.toggle('showNav');
+// show navbtn handler
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navDiv.classList.add("hideNav");
+    navDiv.classList.remove("showNav");
+  });
 });
 
 // stopping animation and transition during window resizing
 let resizeTimer;
-window.addEventListener('resize', () => {
-    document.body.classList.add('resize-animation-stopper');
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => {
-        document.body.classList.remove('resize-animation-stopper');
-    }, 400);
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
 });
